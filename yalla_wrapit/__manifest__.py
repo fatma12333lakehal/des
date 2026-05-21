b291aff871375203c75aca686bd1f15bae7e59ce
FATMA497 <fatima.l@endpointsoft.com>
2026-05-21 21:16:12 +0200

    first commit

---
+++ yalla_wrapit/__manifest__.py
@@ -0,0 +1,32 @@
+{
+    'name': 'yalla',
+    'version': '1.0',
+    'category': 'Project/Engineering',
+    'summary': 'Dynamic Inspection Report for Sales Engineers',
+    'description': """
+        Full dynamic report module for sales engineers.
+        Supports Kitchen, Bathroom, Wardrobes, Doors, Flooring, Furniture.
+        Includes BOQ integration and dynamic scope selection.
+    """,
+    'author': 'Your Name',
+    'depends': ['base', 'project', 'sale','hr','crm'],
+    'data': [
+        'security/yallawrap_security.xml',  # security access rules
+        'security/ir.model.access.csv',               # model access CSV
+
+        'views/crm_lead.xml',
+        'views/floor_view.xml',
+        'views/yallawrap_tile_code.xml',
+        'views/yallawrap_sale_order_views.xml',
+        'reports/report_yallawrap_inspection.xml',
+
+
+
+
+
+
+    ],
+
+    'installable': True,
+    'application': True,
+}
---
+++ yalla_wrapit/models/__init__.py
@@ -0,0 +1,5 @@
+
+
+from . import tile_code
+from . import sale_order
+from . import crm_lead
